9c577d60c31caaade83f5c951da72a3687e09cad
nmsn <136696700@qq.com>
2021-04-15 23:12:43 +0800

    first commit

---
+++ src/configuration.ts
@@ -0,0 +1,16 @@
+import { App, Configuration } from '@midwayjs/decorator';
+import { ILifeCycle } from '@midwayjs/core';
+import { Application } from 'egg';
+import { join } from 'path';
+import * as orm from '@midwayjs/orm';
+
+@Configuration({
+  imports: [orm],
+  importConfigs: [join(__dirname, './config')],
+})
+export class ContainerLifeCycle implements ILifeCycle {
+  @App()
+  app: Application;
+
+  async onReady() {}
+}
